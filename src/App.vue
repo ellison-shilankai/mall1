a3f48dd7338bd58f17071a26ea7c54f19b22ba6b
ellison-shilankai <1067168009@qq.com>
2020-07-19 15:58:05 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,20 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
     <router-view/>
     <main-tab-bar/>
-=======
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
->>>>>>> d3f9fab872930085d0f1179289ec5d9be81c59d2
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
   import MainTabBar from 'components/content/mainTabbar/MainTabBar'
 export default {
   name: 'App',
   components: {
     MainTabBar
-=======
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
->>>>>>> d3f9fab872930085d0f1179289ec5d9be81c59d2
   }
 }
 </script>
 
 <style>
-<<<<<<< HEAD
  @import "assets/css/base.css";
-=======
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
->>>>>>> d3f9fab872930085d0f1179289ec5d9be81c59d2
 </style>
