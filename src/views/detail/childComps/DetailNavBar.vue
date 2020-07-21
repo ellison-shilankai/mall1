390a03e052cb9a1628f772a2fad195d0d47ee33f
shilankai <shilankai@edusoho.com>
2020-07-21 19:02:57 +0800

    商品和商家信息

---
+++ src/views/detail/childComps/DetailNavBar.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <nav-bar>
+      <div slot="left" class="back" @click="backclick">
+        <img src="~assets/img/common/back.svg" alt />
+      </div>
+      <div slot="center" class="title">
+        <div
+          v-for="(item, index) in titles"
+          :key="index"
+          :class="{active: index === currentIndex}"
+          class="title-item"
+          @click="titleClick(index)"
+        >{{item}}</div>
+      </div>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from "components/common/navbar/NavBar";
+export default {
+  name: "DetailNavBar",
+  components: {
+    NavBar
+  },
+  data() {
+    return {
+      titles: ["商品", "参数", "评论", "推荐"],
+      currentIndex: 0
+    };
+  },
+  methods: {
+    titleClick(index) {
+      this.currentIndex = index;
+    },
+    backclick() {
+      // this.$router.go(-1)
+      this.$router.back();
+    }
+  }
+};
+</script>
+
+<style scoped>
+.back img {
+  margin-top: 12px
+}
+
+.title {
+  display: flex;
+  font-size: 13px;
+}
+
+.active {
+  color: var(--color-high-text);
+}
+
+.title-item {
+  flex: 1;
+}
+</style>
---
+++ src/views/detail/childComps/DetailSwiper.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="detail">
+  <swiper class="detaile-swiper">
+    <swiper-item v-for="item in topImages" :key="item.id">
+      <img :src="item" alt />
+    </swiper-item>
+  </swiper>
+  </div>
+</template>
+
+<script>
+import { Swiper, SwiperItem } from "components/common/swiper";
+export default {
+  name: "DetailSwiper",
+  components: {
+    Swiper,
+    SwiperItem
+  },
+  props: {
+    topImages: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.detaile-swiper {
+  height: 300px;
+  overflow: hidden;
+}
+</style>
