77dea05f9970f2e28b3e874b8ac3e430732553db
ellison-shilankai <1067168009@qq.com>
2020-07-22 23:56:57 +0800

    防抖

--- src/components/common/mixin.js
+++ src/components/common/mixin.js
@@ -1,11 +1,11 @@
-import {debounce} from "components/common/utils"
+import {debounce} from "./utils"
 export const itemListenerMixin = {
   mounted() {
-    let newRefresh = debounce(this.$refs.scroll.refresh, 200)
+    let newRefresh = debounce(this.$refs.scroll.refresh, 100)
     this.itemImgListener = () => {
       newRefresh()
     }
-    this.$bus.$on('itemImgLoad', this.itemImgListener)
-    console.log('123')
+    this.$bus.$on('itemImageLoad', this.itemImgListener)
+    // console.log('123')
   }
 }
