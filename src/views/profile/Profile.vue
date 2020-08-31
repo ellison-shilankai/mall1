3a301b2edc32eb9f3be41a71bf0d81278ce22f03
shilankai <shilankai@edusoho.com>
2020-08-31 10:00:08 +0800

    quickSort

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -13,6 +13,7 @@
 <script>
 import Scroll from "components/common/scroll/Scroll";
 import Skeleton from "components/common/skeleton/Skeleton";
+import { quickSort } from "@/components/common/algorithm";
 import { mapState, mapActions } from "vuex";
 export default {
   name: "Profile",
@@ -24,13 +25,18 @@ export default {
     return {
       obj1: { name: "小明", age: 20 },
       obj2: {},
-      obj3: {}
+      obj3: {},
+      arr: [5, 3, 2, 7, 10],
+      arr1: []
     };
   },
   computed: {
     ...mapState(["testBoolean", "testObj"])
   },
   created() {
+    // this.arr1 = quickSort(this.arr);
+    console.log(quickSort(this.arr));
+
     // this.changeStatus(false);
     this.obj2 = this.clone(this.obj1);
     this.obj3 = this.deepClone(this.obj1);
@@ -48,6 +54,26 @@ export default {
     deepClone(origin) {
       let originProto = Object.getPrototypeOf(origin);
       return Object.assign(Object.create(originProto), origin);
+    },
+    quickSort(arr) {
+      if (arr.length <= 1) {
+        return arr;
+      }
+      var pivotIndex = Math.floor(arr.length / 2); //去基准点
+      var pivot = arr.splice(pivotIndex, 1)[0]; //取基准值
+      var left = []; //放小于基准点的值
+      var right = []; //放大于基准点的值
+      //遍历数组，分组
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+          left.push(arr[i]);
+        }
+        if (arr[i] > pivot) {
+          right.push(arr[i]);
+        }
+      }
+      //递归执行以上操作,对左右两个数组进行操作，直到数组长度为<=1；
+      return this.quickSort(left).concat([pivot], this.quickSort(right));
     }
   }
 };
